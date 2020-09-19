afa053e69635728c793ddae457df77828150a285
abdut7 <abdurahmant7@gmail.com>
2020-09-19 14:02:45 +0530

    DSF

--- build/felandingpage.js
+++ build/felandingpage.js
@@ -3,15 +3,20 @@ const path = require('path')
 const app = express();
 try {
     app.use(express.static(path.join(__dirname, 'landingpage')));
-    app.use((req, res, next) => {
-        res.sendFile(__dirname + '/landingpage/index.html')
-        next()
-    })
+    app.use(express.static(path.join(__dirname, 'cartsme')));
+    app.use(express.static(path.join(__dirname, 'dist')));
     app.get('/*', (req, res) => {
-        res.sendFile(__dirname + '/landingpage/index.html')
+        let arrPath = req["path"] ? req["path"].
+        split("/"): ["", ""]
+        if (arrPath[1] == "shop")
+            res.sendFile(__dirname + '/dist/index.html')
+        else if (arrPath[1] == "admin")
+            res.sendFile(__dirname + '/cartsme/index.html')
+        else
+            res.sendFile(__dirname + '/landingpage/index.html')
     })
-    app.listen(4400, function() {
-        console.log('App is listening on port 4400');
+    app.listen(4300, function () {
+        console.log('App is listening on port 4300');
     });
 } catch (error) {
     console.log(error);
